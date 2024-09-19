ade4fc53e80bccf489e69b741900d5827e60dac9
Monika <laiskutis.monikai@gmail.com>
2024-09-19 22:57:07 +0300

    structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Footer } from "../components/footer/Footer";
+import { Header } from "../components/header/Header";
+import { HomeHero } from "../components/hero/HomeHero";
+
+
+export function Home() {
+    return (
+        <>
+            <Header />
+            <HomeHero />
+            <Footer />
+        </>
+    );
+}
