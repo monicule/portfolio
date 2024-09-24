3d2d00f2dd501744dd3b711e71f93dc803b8ae45
Monika <laiskutis.monikai@gmail.com>
2024-09-24 09:36:40 +0300

    icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ export function Footer() {
       <a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
         <svg className="bi" width="30" height="24"><use xlinkHref="#bootstrap"></use></svg>
       </a>
-      <span className="mb-3 mb-md-0 text-body-secondary">© 2024 Company, Inc</span>
+      <span className="mb-3 mb-md-0 text-body-secondary">&copy; 2024 by Monika</span>
     </div>
 
     <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
