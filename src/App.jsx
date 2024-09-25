be455c9d1144cb98014505ee5b3900aa58350642
Monika <laiskutis.monikai@gmail.com>
2024-09-25 12:32:48 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import ScrollToHashElement from "@cascadia-code/scroll-to-hash-element";
 import { Header } from "./components/header/Header.jsx";
 import { Footer } from "./components/footer/Footer.jsx";
 import { HomeHero } from "./components/hero/HomeHero.jsx";
@@ -7,11 +8,12 @@ import { Contacts } from "./components/contacts/Contacts.jsx";
 export function App() {
   return (
     <>
-    < Header />
-    < HomeHero />
-    < Projects />
-    < Contacts />
-    < Footer />
+    <ScrollToHashElement behavior="smooth" inline="center" block="center"/>
+    <Header />
+    <HomeHero />
+    <Projects />
+    <Contacts />
+    <Footer />
     </>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,17 @@
 import style from './Header.module.css';
 import logo from '../../icons/mk.webp';
+import ScrollToHashElement from "@cascadia-code/scroll-to-hash-element";
 
 export function Header() {
     return (
+
        <header className={style.mainHeader}>
+        <ScrollToHashElement />
             <img className={style.logo} src={logo} alt="Logo" />
             <nav className={style.nav}>
                 <a className={style.link} href="#">About me</a>
                 <a className={style.link} href="#">Projects</a>
-                <a className={style.link} href="#">Contacts</a>
+                <a className={style.link} href="/#contactMe">Contacts</a>
             </nav>
         </header>
        
