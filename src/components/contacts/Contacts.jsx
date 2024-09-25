3a2fb6abd25bc8eb5da28d4a726df935e14a128c
Monika <laiskutis.monikai@gmail.com>
2024-09-25 10:32:02 +0300

    up

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -7,8 +7,8 @@ import email from '../../icons/email.svg';
 export function Contacts() {
     return(
         <div className={style.myContacts}>
-        <div className={style.contact}>Contact me: </div>
-        <div className={style.contact} onClick={() => {window.location ='mailto:laiskutis.monikai@gmail.com';}}><img src={email} alt="email" /></div>
+        <div className={style.contactMe}>Contact me: </div>
+        <a target='_top' href='mailto:laiskutis.monikai@gmail.com'><img src={email} alt="email" /></a>
         <div className={style.contact}><a href="https://www.linkedin.com/in/monikakasauskaite/"><img src={linkedin} alt="linkedin" /></a></div>
         <div className={style.contact}><a href="https://github.com/monicule"><img src={github} alt="github" /></a></div>
         <div className={style.contact}><a href="https://www.facebook.com/monicule"><img src={facebook} alt="facebook" /></a></div>
