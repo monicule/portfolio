3e6cf5a3d5607b052ca3ec79757c3cdfc92c798c
Monika <laiskutis.monikai@gmail.com>
2024-09-25 09:38:48 +0300

    contacts

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,11 +1,17 @@
 import style from './Contacts.module.css';
+import linkedin from '../../icons/linkedin.svg';
+import github from '../../icons/github.svg';
+import facebook from '../../icons/facebook.svg';
+import email from '../../icons/email.svg';
 
 export function Contacts() {
     return(
         <div className={style.myContacts}>
-        <div className={style.contact}>E-mail: laiskutis.monikai@gmail.com</div>
-        <div className={style.contact}><a href="https://www.linkedin.com/in/monikakasauskaite/">Linkedin</a></div>
-        <div className={style.contact}><a href="https://github.com/monicule">GitHub</a></div>
+        <div className={style.contact}>Contact me: </div>
+        <div className={style.contact} onClick={() => {window.location ='mailto:laiskutis.monikai@gmail.com';}}><img src={email} alt="email" /></div>
+        <div className={style.contact}><a href="https://www.linkedin.com/in/monikakasauskaite/"><img src={linkedin} alt="linkedin" /></a></div>
+        <div className={style.contact}><a href="https://github.com/monicule"><img src={github} alt="github" /></a></div>
+        <div className={style.contact}><a href="https://www.facebook.com/monicule"><img src={facebook} alt="facebook" /></a></div>
         </div>
 
     );
